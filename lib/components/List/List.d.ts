bd507c8e1461a75683aaebeb279825497097bea6
Ardian Shala <ardian.sh@outlook.de>
2021-03-15 23:58:02 +0100

    v2.3.1

--- lib/components/List/List.d.ts
+++ lib/components/List/List.d.ts
@@ -1,9 +1,8 @@
 import { ReactElement } from 'react';
 import { IListItemProps } from './ListItem';
-interface IListProps {
+export interface IListProps {
     children?: ReactElement<IListItemProps> | Array<ReactElement<IListItemProps>>;
     isFlush?: boolean;
     isHoverable?: boolean;
 }
 export declare const List: (props: IListProps) => JSX.Element;
-export {};
