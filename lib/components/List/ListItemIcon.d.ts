e825402f00614b813dd091861215bd087b7acec8
Ardian Shala <ardian.sh@outlook.de>
2021-03-15 09:37:39 +0100

    v2.3.0

---
+++ lib/components/List/ListItemIcon.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+interface IListItemIconProps {
+    icon: React.SVGProps<SVGSVGElement>;
+}
+export declare const ListItemIcon: ({ icon }: IListItemIconProps) => JSX.Element;
+export {};
--- lib/components/List/index.d.ts
+++ lib/components/List/index.d.ts
@@ -1,5 +1,6 @@
 export * from './List';
 export * from './ListItem';
 export * from './ListItemAvatar';
+export * from './ListItemIcon';
 export * from './ListItemAction';
 export * from './ListItemText';
