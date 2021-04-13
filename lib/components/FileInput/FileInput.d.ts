4877592d7772b63ca2bae7b7d76f6f57bc484185
Ardian Shala <ardian.sh@outlook.de>
2021-04-13 11:45:40 +0200

    v2.10.0

--- lib/components/FileInput/FileInput.d.ts
+++ lib/components/FileInput/FileInput.d.ts
@@ -1,6 +1,4 @@
 import React from 'react';
 export interface IFileInputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
-    name?: string;
-    value?: string;
 }
 export declare const FileInput: (props: IFileInputProps) => JSX.Element;
