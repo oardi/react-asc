5350042bbaf3db607b4c10efde9cdf130556d109
Ardian Shala <ardian.sh@outlook.de>
2021-06-10 13:07:03 +0200

    cleanup

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -2,5 +2,4 @@ export * from './AppInfo';
 export * from './AppSidebar';
 export * from './ButtonCounter';
 export * from './Markdown';
-export * from './Highlight';
 export * from './AppBreadcrumb';
