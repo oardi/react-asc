45c12fa8e28abd61e1640db74ea9d18455d3a45a
Ardian Shala <ardian.sh@outlook.de>
2021-07-12 07:46:07 +0200

    v8.1.2

--- lib/components/Typography/Typography.d.ts
+++ lib/components/Typography/Typography.d.ts
@@ -1,12 +1,13 @@
 import { ReactElement } from 'react';
 export interface IWrapperProps {
-    as: string;
+    as?: string;
     children: ReactElement;
     className?: string;
 }
 export interface ITypographyProps {
-    as: string;
+    as?: string;
     children: any;
     className?: string;
+    style?: any;
 }
 export declare const Typography: ({ children, as, ...rest }: ITypographyProps) => JSX.Element;
