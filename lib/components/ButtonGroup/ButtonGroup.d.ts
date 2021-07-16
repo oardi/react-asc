f7cb10e15cf7c678d7db08edde024e8b56fab05e
Ardian Shala <ardian.sh@outlook.de>
2021-07-16 22:11:19 +0200

    v10.0.0

---
+++ lib/components/ButtonGroup/ButtonGroup.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+export interface IButtonGroupProps extends React.DetailedHTMLProps<React.HtmlHTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+}
+export declare const ButtonGroup: (props: IButtonGroupProps) => JSX.Element;
