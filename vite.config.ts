6afd9c66540fd1fb72de34b85a2a83c6d4d67713
Ardian Shala <ardian.sh@outlook.de>
2022-01-23 11:28:21 +0100

    changed pwa settings and icons

--- vite.config.ts
+++ vite.config.ts
@@ -19,19 +19,17 @@ export default defineConfig({
 						"src": "pwa-192x192.png",
 						"sizes": "192x192",
 						"type": "image/png",
-						"purpose": "maskable any"
 					},
 					{
 						"src": "pwa-512x512.png",
 						"sizes": "512x512",
 						"type": "image/png",
-						"purpose": "maskable any"
 					},
 					{
 						src: 'pwa-512x512.png',
 						sizes: '512x512',
 						type: 'image/png',
-						purpose: 'any maskable',
+						purpose: 'any maskable'
 					}
 				],
 				"start_url": ".",
