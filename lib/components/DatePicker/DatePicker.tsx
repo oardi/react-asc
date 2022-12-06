3189018bcf0278a7d4cbfececeb687eb682d3db0
Ardian Shala <ardian.sh@outlook.de>
2022-12-06 21:03:13 +0200

    added datepicker upgraded packages

--- lib/components/DatePicker/DatePicker.tsx
+++ lib/components/DatePicker/DatePicker.tsx
@@ -1,52 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import { loggerService } from 'src/shared';
+import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-
-export enum DATEMODE { YEAR, MONTH, DAY }
+import styles from './DatePicker.module.scss';
 
 export interface IDatePickerProps {
 	value?: Date;
 	selectRange?: boolean;
+	minDate?: Date;
+	maxDate?: Date;
 	// className?: string;
 	// disabled?: boolean;
-	// yearConfig?: { from?: number, to?: number };
-	// onChange?: (val: Date) => void;
+	onChange?: (val: Date) => void;
 }
 
 export const DatePicker = (props: IDatePickerProps): JSX.Element => {
 
-	const { value = new Date(), selectRange } = props;
+	const {
+		value = new Date(),
+		selectRange,
+		minDate,
+		maxDate,
+		// disabled,
+		onChange,
+		...rest
+	} = props;
 	const [currDate, setCurrDate] = useState<Date>(value);
 
-	useEffect(() => {
-		init();
-	}, []);
-
-	// sun, mon, tue
-	const init = () => {
-		const currYear: number = currDate.getFullYear();
-		const currMonth: number = currDate.getMonth();
-		const currday: number = currDate.getDate();
-		const firstWeekday: number = new Date(currYear, currMonth, 1).getDay();
-		const lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate()
-
-		loggerService.warn(currYear, currMonth, currday, firstWeekday, lastDateOfMonth);
+	const handleOnChange = (e: Date): void => {
+		setCurrDate(e);
+		onChange && onChange(e);
 	};
 
-	// get current date
-	// get weeks of month
-	// for each week show each day in week
-
-	// month = 1 to end of month
-
 	return (
-		<>
-			<Calendar
-				value={value}
-				selectRange={selectRange}
-				onChange={setCurrDate}
-			/>
-		</>
+		<Calendar
+			className={styles.datePicker}
+			value={currDate}
+			minDate={minDate}
+			maxDate={maxDate}
+			selectRange={selectRange}
+			onChange={handleOnChange}
+			{...rest}
+		/>
 	);
 };
--- src/main/examples/DatePickerPage.tsx
+++ src/main/examples/DatePickerPage.tsx
@@ -1,17 +1,56 @@
-import React, { useEffect } from 'react';
-import type { IDatePickerProps, IDateSelectProps } from 'lib';
+import React, { useEffect, useState } from 'react';
+import type { IDatePickerProps } from 'lib';
+import { Button, Menu } from 'lib';
+import { FormControl } from 'lib';
 import { DatePicker } from 'lib';
-import type { IShowcaseBaseProps} from './components';
+import type { IShowcaseBaseProps } from './components';
 import { withOptions } from './components';
 
-const DatePickerPageBase = ({ setSettingsControls }: IShowcaseBaseProps<IDateSelectProps>): JSX.Element => {
+const DatePickerPageBase = ({ settingValues, setSettingsControls }: IShowcaseBaseProps<IDatePickerProps>): JSX.Element => {
 
-	useEffect(() => setSettingsControls({}), []);
+	useEffect(() => setSettingsControls({
+		selectRange: new FormControl(settingValues.selectRange, [], 'checkbox', { label: 'select range?' }),
+		minDate: new FormControl('1970-01-01', [], 'date', { label: 'Min Date' }),
+		maxDate: new FormControl(new Date(), [], 'date', { label: 'Max Date' }),
+	}), []);
+
+	const [currDate, setCurrDate] = useState<Date>(new Date());
+	const [open, setOpen] = useState(false);
+
+	const handleClickDateInMenu = (val: Date): void => {
+		console.warn(val);
+		setCurrDate(val);
+		setOpen(false);
+	};
 
 	return (
-		<DatePicker
-			selectRange={true}
-		/>
+		<>
+			{settingValues.minDate} {typeof settingValues.minDate}
+
+			<DatePicker
+				minDate={settingValues.minDate}
+				selectRange={settingValues.selectRange}
+				onChange={setCurrDate}
+			/>
+
+			<p>
+				selected date: {currDate?.toString()}
+			</p>
+
+			<Menu
+				toggle={
+					<Button onClick={(): void => setOpen(true)}>
+						show Datepicker in menu
+					</Button>
+				}
+				open={open}
+				onClickBackdrop={(): void => setOpen(false)}
+			>
+				<DatePicker
+					onChange={(val: Date): void => handleClickDateInMenu(val)}
+				/>
+			</Menu>
+		</>
 	);
 };
 
