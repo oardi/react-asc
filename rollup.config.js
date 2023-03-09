e0821e3e1c6addcf1ebf2c0b7f166b80a504c7d5
Ardian Shala <ardian.sh@outlook.de>
2023-03-09 15:33:06 +0200

    upgraded packages fixed build

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
-import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
-import external from 'rollup-plugin-peer-deps-external';
-import resolve from 'rollup-plugin-node-resolve';
 import json from '@rollup/plugin-json';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
+import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
 const rollupConfig = {
@@ -26,11 +26,13 @@ const rollupConfig = {
 			file: pkg.main,
 			format: 'cjs',
 			exports: 'named',
+			sourcemap: true,
 		},
 		{
 			file: pkg.module,
 			format: 'es',
 			exports: 'named',
+			sourcemap: true,
 		},
 	],
 
