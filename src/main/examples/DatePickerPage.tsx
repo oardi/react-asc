8526462b01f70e4ce98fe78ba00300b8067f8982
Ardian Shala <ardian.sh@outlook.de>
2023-11-06 11:12:29 +0200

    cleanup datepicker page

--- src/main/examples/DatePickerPage.tsx
+++ src/main/examples/DatePickerPage.tsx
@@ -1,18 +1,16 @@
-import React, { useEffect, useState } from 'react';
 import type { IDatePickerProps } from 'lib';
-import { Button, Menu } from 'lib';
-import { FormControl } from 'lib';
-import { DatePicker } from 'lib';
+import { Button, DatePicker, Menu } from 'lib';
+import { useEffect, useState } from 'react';
 import type { IShowcaseBaseProps } from './components';
 import { withOptions } from './components';
 
-const DatePickerPageBase = ({ settingValues, setSettingsControls }: IShowcaseBaseProps<IDatePickerProps>): JSX.Element => {
+const DatePickerPageBase = ({ setSettingsControls }: IShowcaseBaseProps<IDatePickerProps>): JSX.Element => {
 	useEffect(
 		() =>
 			setSettingsControls({
-				selectRange: new FormControl(settingValues.selectRange, [], 'checkbox', { label: 'select range?' }),
-				minDate: new FormControl('1970-01-01', [], 'date', { label: 'Min Date' }),
-				maxDate: new FormControl(new Date(), [], 'date', { label: 'Max Date' }),
+				// selectRange: new FormControl(settingValues.selectRange, [], 'checkbox', { label: 'select range?' }),
+				// minDate: new FormControl(new Date('1970-01-01'), [], 'date', { label: 'Min Date' }),
+				// maxDate: new FormControl(new Date(), [], 'date', { label: 'Max Date' }),
 			}),
 		[]
 	);
@@ -21,15 +19,13 @@ const DatePickerPageBase = ({ settingValues, setSettingsControls }: IShowcaseBas
 	const [open, setOpen] = useState(false);
 
 	const handleClickDateInMenu = (val: Date): void => {
-		console.warn(val);
 		setCurrDate(val);
 		setOpen(false);
 	};
 
 	return (
 		<>
-			{settingValues.minDate} {typeof settingValues.minDate}
-			<DatePicker minDate={settingValues.minDate} selectRange={settingValues.selectRange} onChange={setCurrDate} />
+			<DatePicker onChange={setCurrDate} />
 			<p>selected date: {currDate?.toString()}</p>
 			<Menu
 				toggle={<Button onClick={(): void => setOpen(true)}>show Datepicker in menu</Button>}
